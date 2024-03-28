287631ad5818b862cb4135b0881ef55cc47c84f8
Hadson00 <hadson798@gmail.com>
2024-03-27 22:12:04 -0300

    AddEx

---
+++ ex1.py
@@ -0,0 +1,9 @@
+'''A classe Avião poderia ter uma associação com a classe Coordenada Geográfica, pois
+esta ultima consegue representar a latitude, longitude e permite ainda indicar a
+distância em km entre duas Coordenada Geográfica
+A associação entre essas classes seria do tipo Agregação ou Composição?
+Quais atributos e métodos deverão permanecer na classe Avião?'''
+
+class Coordenadas:
+    def __init__(self):
+        pass
---
+++ ex2.py
@@ -0,0 +1,4 @@
+'''Crie um programa para representar funcionários em um sistema de gestão de
+pessoas. 
+É necessário que nesta modelagem seja feito de uso de no mínimo duas classes e
+com uma associação entre estas.'''
